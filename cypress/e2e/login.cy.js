07bab87ca263715baa276412da1013fc63af330c
Aiala <pricerqueira32@gmail.com>
2025-08-02 17:01:32 -0300

    Adicionei dados provenientes do fixture e alterei o .gitignore

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -5,20 +5,23 @@ describe('Login', () => {
   })
 
   it('Login com dados válidos devem permitir entrada no sistema', () => {
-    cy.get('#username').click().type('julio.lima')
-    cy.get('#senha').click().type('123456')
+    cy.fixture('credenciais').then(credenciais => {
+    cy.get('#username').click().type(credenciais.valida.usuario)
+    cy.get('#senha').click().type(credenciais.valida.senha)
+    })
+    
     cy.screenshot('apos-preencher-dados-validos')
     cy.contains('button', 'Entrar').click()
     cy.screenshot('apos-clicar-no-botao-entrar')
-
   
     cy.contains('h4', 'Realizar Transferência').should('be.visible')
-    
   })
 
   it('Login com dados inválidos devem apresentar mensagem de erro', () => {
-    cy.get('#username').click().type('julio.lima')
-    cy.get('#senha').click().type('654321')
+    cy.fixture('credenciais').then(credenciais => {
+    cy.get('#username').click().type(credenciais.invalida.usuario)
+    cy.get('#senha').click().type(credenciais.invalida.senha)
+    })
     cy.contains('button', 'Entrar').click()
       
     cy.get('.toast').should('have.text', 'Erro no login. Tente novamente.')

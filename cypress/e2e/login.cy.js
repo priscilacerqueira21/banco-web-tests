47518b39aecb8f23ca2fde67dd47d26a265bdd0e
Aiala <pricerqueira32@gmail.com>
2025-08-02 16:49:21 -0300

    Adicionei  screenshots

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,12 +1,16 @@
 describe('Login', () => {
   beforeEach(() => {
     cy.visit('http://localhost:4000')
+    cy.screenshot('apos-visitar-pagina')
   })
 
   it('Login com dados válidos devem permitir entrada no sistema', () => {
     cy.get('#username').click().type('julio.lima')
     cy.get('#senha').click().type('123456')
+    cy.screenshot('apos-preencher-dados-validos')
     cy.contains('button', 'Entrar').click()
+    cy.screenshot('apos-clicar-no-botao-entrar')
+
   
     cy.contains('h4', 'Realizar Transferência').should('be.visible')
     
